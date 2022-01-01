c5fbea4eb6f303a535c62f43aaf34d5bf9848fbf
Mhedii <mehedi1234.mh@gmail.com>
2022-01-01 19:16:04 +0600

    Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import AuthProvider from './Components/Context/AuthProvider';
+import Home from './Components/Pages/Home/Home';
+import NavBar from './Components/Pages/Home/NavBar';
 
 function App() {
   return (
     <AuthProvider>
       <div className="App">
-      
+        <NavBar></NavBar>
+      <Home></Home>
       </div>
     </AuthProvider>
   );
