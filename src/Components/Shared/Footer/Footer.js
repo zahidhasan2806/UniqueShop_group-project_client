36d7a476f5a46f2100a1f11a93b2cc65b25a391a
coderSabbirr <sabirnahid5817@gmail.com>
2022-01-02 10:01:20 +0600

    footer add

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -2,96 +2,97 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.css';
 const Footer = () => {
-    return ( 
-        <>
-        <div>
-        <div className="container all-menu">
-            <div className="row">
-            <div className="contect col-lg-3 col-md-6 col-12  order-lg-1 ">
-                <h3><i class="fas fa-headphones"></i><span className="ms-1">GOT QUESTIONS ?</span></h3>
-                <p >(001) 2345-6789</p>
-                <p>(001) 5823-6530</p>
-                <p><i class="fas fa-map-marker-alt"></i><span className="ms-1">184 Main Rd E, St Albans<br/>
-                    VIC 3021, Australia</span></p>
-                <a href="mailto:contact@company.com"><i class="fas fa-envelope"></i><span className="ms-1">contact@company.com</span></a>
-            </div>
-            <div className="col-lg-2 col-md-6 col-12  order-lg-1 ">
-                
-                <ul className="menu">
-                <h3>Categories</h3>
-                    <li className="menu-item">
-                        <Link to="/">Men</Link>
-                    </li>
-                    <li className="menu-item">
-                        <Link to="/">Women</Link>
-                    </li>
-                    <li className="menu-item">
-                        <Link to="/">Accessories</Link>
-                    </li>
-                    <li className="menu-item">
-                        <Link to="/">Shoes</Link>
-                    </li>
-                    <li className="menu-item">
-                        <Link to="/">Denim</Link>
-                    </li>
-                </ul>
-            </div>
-            <div className="col-lg-2 col-md-6 col-12  order-lg-1 ">
-                
-                <ul className="menu">
-                <h3>Infomation</h3>
-                    <li className="menu-item">
-                        <Link to="/about">About Us</Link>
-                    </li>
-                    <li className="menu-item">
-                        <Link to="/contact">Contact Us</Link>
-                    </li>
-                    <li className="menu-item">
-                        <Link to="/">Terms & Conditions</Link>
-                    </li>
-                    <li className="menu-item">
-                        <Link to="/">Returns & Exchanges</Link>
-                    </li>
-                    <li className="menu-item">
-                        <Link to="/">Shipping & Delivery</Link>
-                    </li>
-                </ul>
-            </div>
-            <div className="col-lg-2 col-md-6 col-12  order-lg-1 ">
-               
-                <ul className="menu">
-                <h3>Useful links</h3>
-                    <li className="menu-item">
-                        <Link to="/">Store Location</Link>
-                    </li>
-                    <li className="menu-item">
-                        <Link to="/dashboard">My Account</Link>
-                    </li>
-                    <li className="menu-item">
-                        <Link to="/">Latest News</Link>
-                    </li>
-                    <li className="menu-item">
-                        <Link to="/">Size Guide</Link>
-                    </li>
-                    <li className="menu-item">
-                        <Link to="/">FAQs</Link>
-                    </li>
-                </ul>
-            </div>
-            <div className="col-lg-3 col-md-6 col-12  order-lg-1 ">
-                <h3><i class="far fa-money-bill-alt"></i><span className="ms-1">GOT QUESTIONS ? WE ARE USING SAFE PAYMENTS</span></h3>
-                <li><img src="https://i.ibb.co/JnbCY6t/PROD1-500x.png" alt="payment" className="payment mb-2 mt-2" /></li>
-                <li><img src="https://i.ibb.co/fHWBpR7/payment.png" alt="Secured" className="secured mt-2" /></li>
-               
-            </div>
-            </div>
+    return (
+        
+            <div className="footer">
+                <div className="container all-menu">
+                    <div className="row">
+                        <div className="contect col-lg-3 col-md-6 col-12  order-lg-1 ">
+                            <h3><i class="fas fa-headphones"></i><span className="ms-1">GOT QUESTIONS ?</span></h3>
+                            <p >(001) 2345-6789</p>
+                            <p>(001) 5823-6530</p>
+                            <p><i class="fas fa-map-marker-alt"></i><span className="ms-1">184 Main Rd E, St Albans<br />
+                                VIC 3021, Australia</span></p>
+                            <a href="mailto:contact@company.com"><i class="fas fa-envelope"></i><span className="ms-1">contact@company.com</span></a>
+                        </div>
+                        <div className="col-lg-2 col-md-6 col-12  order-lg-1 ">
 
-        </div>
-        </div>
-        <div>
-            <h2>hi</h2>
-        </div>
-        </>
+                            <ul className="menu">
+                                <h3>Categories</h3>
+                                <li className="menu-item">
+                                    <Link to="/">Men</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/">Women</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/">Accessories</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/">Shoes</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/">Denim</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-lg-2 col-md-6 col-12  order-lg-1 ">
+
+                            <ul className="menu">
+                                <h3>Infomation</h3>
+                                <li className="menu-item">
+                                    <Link to="/about">About Us</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/contact">Contact Us</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/">Terms & Conditions</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/">Returns & Exchanges</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/">Shipping & Delivery</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-lg-2 col-md-6 col-12  order-lg-1 ">
+
+                            <ul className="menu">
+                                <h3>Useful links</h3>
+                                <li className="menu-item">
+                                    <Link to="/">Store Location</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/dashboard">My Account</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/">Latest News</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/">Size Guide</Link>
+                                </li>
+                                <li className="menu-item">
+                                    <Link to="/">FAQs</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-12  order-lg-1 ">
+                            <h3><i class="far fa-money-bill-alt"></i><span className="ms-1">GOT QUESTIONS ? WE ARE USING SAFE PAYMENTS</span></h3>
+                            <li><img src="https://i.ibb.co/JnbCY6t/PROD1-500x.png" alt="payment" className="payment mb-2 mt-2" /></li>
+                            <li><img src="https://i.ibb.co/fHWBpR7/payment.png" alt="Secured" className="secured mt-2" /></li>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div>
+                <p className="copyright">Copyright © 2022  all rights reserved.</p>
+            </div>
+            </div>
+           
+      
     );
 };
 
