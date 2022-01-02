ee6403e645f008abc7347f0d1409fa02679c5d44
Mhedii <mehedi1234.mh@gmail.com>
2022-01-02 20:20:09 +0600

    Store Location

---
+++ src/Components/Pages/StoreLocation/StoreLocation.js
@@ -0,0 +1,86 @@
+import React from 'react';
+
+const StoreLocation = () => {
+    return (
+        <div>
+            <div><h1 className='p-5 text-white' style={{ background: "#6e7a71" }}>Store Location</h1></div>
+            <div class="container">
+                <div class="row text-start">
+                    <div class="col">
+                        <div className='mt-4'>
+                            <h3 className='fw-bold'>BULGARIA</h3>
+                            <h4>Mall Varna</h4>
+                            <p>186 Vladislav Varnenchik Blvd.2nd floor, store No.18 Varna 9000</p>
+                            <p>Tel: +359-893 695881</p>
+                        </div>
+                        <div className='mt-4'>
+                            <h3 className='fw-bold'>DUBAI</h3>
+                            <h4>Mall of the Emirates</h4>
+                            <p>4th Interchange, Sheikh Zayed Road 1st Level</p>
+                            <p>Tel: +971-4 341 3666</p>
+                        </div>
+                        <div className='mt-4'>
+
+                            <h4>Mercato</h4>
+                            <p>Jumeirah Beach Rd 1st Level</p>
+                            <p>Tel: +971-4 344 0015</p>
+                        </div>
+                        <div className='mt-4'>
+                            <h3 className='fw-bold'>INDONESIA</h3>
+                            <h4>Central Park</h4>
+                            <p>UG Floor Unit-123A, Jl. Let Jend S.Parman Kav.28, Jakarta Barat 11470</p>
+                            <p>Tel: +62-21 29200095</p>
+                        </div>
+                        <div className='mt-4'>
+
+                            <h4>Paris Van Java</h4>
+                            <p>Resort Level #D-06, Jl. Sukajadi No.137-139, Bandung 40162</p>
+                            <p>Tel: +62-22 82063875</p>
+                        </div>
+                        <div className='mt-4'>
+
+                            <h4>Gandaria City</h4>
+                            <p>UG Floor Unit 61, Jl. KH Syafi¡¯I Hadzami, Jakarta Selatan 12240</p>
+                            <p>Tel: +62-21 29007806</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div className='mt-5'>
+
+                            <h4>Central pedestrian area</h4>
+                            <p>23 Knyaz Boris I Blvd Varna 9000</p>
+                            <p>Tel: +359-893 695882</p>
+                        </div>
+                        <div className='mt-5'>
+
+                            <h4>Deira City Centre</h4>
+                            <p>8th Street Deira 1st Level</p>
+                            <p>Tel: +971-4 295 0790</p>
+                        </div>
+                        <div className='mt-5'>
+
+                            <h4>Seminyak</h4>
+                            <p>Jl. Raya Seminyak No.9, Bali 80361</p>
+                            <p>Tel: +62-361 733462</p>
+                        </div>
+                        <div className='mt-5'>
+
+                            <h4>Pondok Indah Mall 2</h4>
+                            <p>1st Floor #127, Jl. Metro Pondok Indah Blok 3B, Jakarta Selatan 12310</p>
+                            <p>Tel: +62-21 75920987</p>
+                        </div>
+                        <div className='mt-5'>
+
+                            <h4>Galaxy Mall</h4>
+                            <p>1st Floor, Unit 125-126, Jl. Dharma Husada Indah Timur No.37, Surabaya 60115</p>
+                            <p>Tel: +62-31 5937282</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default StoreLocation;
